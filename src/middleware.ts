edf81dcd24dd7f1f8658fee16186af4d85798e48
Casey K <tehcnicolour@gmail.com>
2023-05-06 13:45:15 -0700

    post prisma schema

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,5 +3,19 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware();
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)"],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next
+     * - static (static files)
+     * - favicon.ico (favicon file)
+     * - public folder
+     */
+    "/((?!static|.*\\..*|_next|favicon.ico).*)",
+    "/",
+  ],
 };
+
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)"],
+// };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,24 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      'primary': "#00ff00",
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#00ff00',
+      'yellow': '#ffc82c',
+      'black': '#000000',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      'white': '#ffffff',
+    },
+    fontFamily: {
+      sans: ['Arial', 'sans-serif'],
+      mono: ['JetBrains Mono', 'monospace'],
+    },
     extend: {},
   },
   plugins: [],
